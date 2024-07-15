f82b41703b39400349246689f01e4bf80e07b1dd
Your Name <your_email@example.com>
2024-07-15 21:43:48 +0300

    last cmmit

--- lib/core/utlis/styles.dart
+++ lib/core/utlis/styles.dart
@@ -1,42 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 
 abstract class Styles {
   static const textStyle18 = TextStyle(
-    fontSize: 15,
-    fontWeight: FontWeight.bold,
-    color: Colors.white,
-  );
-
-  static const textStyle20 = TextStyle(
-    fontSize: 20,
-    fontWeight: FontWeight.w400,
-    color: Colors.white,
-  );
-
-  static const textStyle14 = TextStyle(
-    fontSize: 13,
+    fontSize: 18,
     fontWeight: FontWeight.w800,
     color: Colors.white,
+    decoration: TextDecoration.none,
   );
-
-  static const ratingCount = TextStyle(
-    fontSize: 14,
-    fontWeight: FontWeight.w200,
+  static const textStyle20 = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.normal,
     color: Colors.white,
+    decoration: TextDecoration.none,
   );
-
+  static const textStyle14 = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.normal,
+    color: Colors.white,
+    decoration: TextDecoration.none,
+  );
   static const textStyle16 = TextStyle(
-    fontSize: 18,
+    fontSize: 16,
     fontWeight: FontWeight.w500,
     color: Colors.white,
+    decoration: TextDecoration.none,
   );
-
+  static const ratingCount = TextStyle(
+    fontSize: 12,
+    fontWeight: FontWeight.w200,
+    color: Colors.white,
+    decoration: TextDecoration.none,
+  );
   static const pricestyle = TextStyle(
-    fontSize: 18,
+    fontSize: 16,
     fontWeight: FontWeight.w900,
     color: Colors.white,
+    decoration: TextDecoration.none,
   );
 }
+
+
+
+
 
 const TextStyle textStyle = TextStyle();
 const BackgroundColor = Color(0xff100B20);
