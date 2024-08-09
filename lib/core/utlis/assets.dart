0af66eed8fbb15564e62bd6db79b9b8b3c8b6fbe
Your Name <your_email@example.com>
2024-08-09 13:27:08 +0300

    geting data from api to list view is done

--- lib/core/utlis/assets.dart
+++
@@ -1,6 +0,0 @@
-class AssetsData {
-  static const logo = 'assets/images/Logo.png';
-  static const testImage = 'assets/images/test_image.png';
-  static const hom = 'assets/images/hom.jpg';
-
-}
--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,63 @@
-import 'package:bookly/core/utlis/styles.dart';
-import 'package:bookly/features/splash/presentation/views/splash_screen.dart';
+import 'package:bookly/Features/home/data/repos/home_repo_impl.dart';
+import 'package:bookly/Features/home/domain/entities/book_entity.dart';
+import 'package:bookly/Features/home/domain/use_cases/fetch_featured_books_use_case.dart';
+import 'package:bookly/Features/home/domain/use_cases/fetch_newest_books_use_case.dart';
+import 'package:bookly/Features/home/presentation/manger/featured_books_cubit/featured_books_cubit.dart';
+import 'package:bookly/Features/home/presentation/manger/newset_books_cubit/newest_books_cubit.dart';
+import 'package:bookly/constants.dart';
+import 'package:bookly/core/utils/app_router.dart';
+import 'package:bookly/core/utils/simple_bloc_observer.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:hive_flutter/adapters.dart';
 
-void main() {
-  runApp(MyApp());
+import 'core/utils/functions/setup_service_locator.dart';
+
+void main() async {
+  await Hive.initFlutter();
+  Hive.registerAdapter(BookEntityAdapter());
+  setupServiceLocator();
+  await Hive.openBox<BookEntity>(kFeaturedBox);
+  await Hive.openBox<BookEntity>(kNewestBox);
+  Bloc.observer = SimpleBlocObserver();
+  runApp(const Bookly());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class Bookly extends StatelessWidget {
+  const Bookly({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: const SplashScreen(),
-      theme: ThemeData.dark().copyWith(
-        textTheme: GoogleFonts.montserratTextTheme(
-          ThemeData.dark().textTheme,
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) {
+            return FeaturedBooksCubit(
+              FetchFeaturedBooksUseCase(
+                getIt.get<HomeRepoImpl>(),
+              ),
+            )..fetchFeaturedBooks();
+          },
         ),
-        scaffoldBackgroundColor: BackgroundColor,
+        BlocProvider(
+          create: (context) {
+            return NewestBooksCubit(
+              FetchNewestdBooksUseCase(
+                getIt.get<HomeRepoImpl>(),
+              ),
+            );
+          },
+        )
+      ],
+      child: MaterialApp.router(
+        routerConfig: AppRouter.router,
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData.dark().copyWith(
+          scaffoldBackgroundColor: kPrimaryColor,
+          textTheme:
+              GoogleFonts.montserratTextTheme(ThemeData.dark().textTheme),
+        ),
       ),
     );
   }
